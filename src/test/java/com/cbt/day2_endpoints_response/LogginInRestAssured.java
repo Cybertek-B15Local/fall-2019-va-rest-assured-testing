0d421b92e01ad7334375b54f9782fbb5ac2716d0
marufjont <marufjont@gmail.com>
2020-05-20 20:04:45 -0400

    LogginInRestAssured.java

--- src/test/java/com/cbt/day2_endpoints_response/LogginInRestAssured.java
+++ src/test/java/com/cbt/day2_endpoints_response/LogginInRestAssured.java
@@ -2,11 +2,29 @@ package com.cbt.day2_endpoints_response;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class LogginInRestAssured {
 
     @BeforeAll
     public static void setup(){
         RestAssured.baseURI = "http://api.openrates.io";
+    }
+
+    @Test
+    public void test1(){
+        // request logging
+        RestAssured.
+                given().log().everything().
+                when().get("latest").
+                then().statusCode(200);
+    }
+
+    @Test
+    public void test2(){
+        RestAssured.
+                when().get("latest").
+                then().log().all().
+                    statusCode(200);
     }
 }
