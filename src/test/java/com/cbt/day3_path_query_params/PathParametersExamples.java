92f636c98cb949b40ae25baf1885cd23f4c3baa1
marufjont <marufjont@gmail.com>
2020-05-21 19:19:56 -0400

    path parameters example

---
+++ src/test/java/com/cbt/day3_path_query_params/PathParametersExamples.java
@@ -0,0 +1,48 @@
+package com.cbt.day3_path_query_params;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class PathParametersExamples {
+
+    @BeforeAll
+    public static void setUp(){
+        RestAssured.baseURI = "http://api.cybertektraining.com";
+    }
+
+    /**
+     * make a get request to /student/:id and get single student information
+     * provide the path parameters
+     * verify status code
+     * verify student name
+     */
+    @Test
+    public void testVasylFromIndia(){
+        given().
+                log().all().
+                pathParam("id", 10521).
+        when().
+                get("/student/{id}").
+                prettyPeek().
+        then().statusCode(200);
+    }
+
+    /**
+     * same test as above but with a different id
+     */
+    @Test
+    public void testVera(){
+        given().
+                log().all().
+                pathParam("id", 10662).
+        when().
+                get("student/{id}").
+                prettyPeek().
+        then().statusCode(200);
+    }
+
+}
