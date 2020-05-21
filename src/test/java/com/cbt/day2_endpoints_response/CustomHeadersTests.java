5c985441cd47a4149326a970c570b68b71479d86
marufjont <marufjont@gmail.com>
2020-05-20 21:11:50 -0400

    CustomHeadersTests.java

--- src/test/java/com/cbt/day2_endpoints_response/CustomHeadersTests.java
+++ src/test/java/com/cbt/day2_endpoints_response/CustomHeadersTests.java
@@ -1,11 +1,20 @@
 package com.cbt.day2_endpoints_response;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class CustomHeadersTests {
+import static io.restassured.RestAssured.given;
 
+public class CustomHeadersTests {
+/*
+Accept --> is a header that is part of request.
+It is used to indicate in what format we want the response in
+if i want the response to json, i send with header application/json
+
+then response will be in json and it will have header content-type with value json
+ */
     @BeforeAll
     public static void setup(){
         RestAssured.baseURI = "http://54.224.118.38:8000/api";
@@ -19,9 +28,15 @@ public class CustomHeadersTests {
 
     @Test
     public void xmlTest(){
-
+        given().
+                log().all().
+                header("Accept", "application/xml").
+        when().
+                get("/spartans").
+        then().
+                log().all().
+                contentType(ContentType.XML);
     }
-
 
     /**
      * send a get request to http://54.224.118.38:8000/api
