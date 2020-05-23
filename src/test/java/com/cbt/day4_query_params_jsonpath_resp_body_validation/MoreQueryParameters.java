c01965b3fd55b68173a4adaca7a83222827ce100
marufjont <marufjont@gmail.com>
2020-05-23 10:35:08 -0400

    query parameters example

--- src/test/java/com/cbt/day4_query_params_jsonpath_resp_body_validation/MoreQueryParameters.java
+++ src/test/java/com/cbt/day4_query_params_jsonpath_resp_body_validation/MoreQueryParameters.java
@@ -4,6 +4,9 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class MoreQueryParameters {
@@ -34,6 +37,34 @@ public class MoreQueryParameters {
      */
     @Test
     public void baseAndSymbolsTest(){
+        given().
+                log().all().
+                queryParam("symbols", "USD").
+                queryParam("base", "PHP").
+        when().
+                get("/latest").
+                prettyPeek().
+        then().
+                statusCode(200);
+    }
+
+    /*
+    base  PHP
+    symbols USD
+     */
+    @Test
+    public void baseAndSymbolsTestWithMap(){
+        Map<String, String> parametersMap=new HashMap<>();
+        parametersMap.put("base", "PHP");
+        parametersMap.put("symbols", "USD");
+
+        given().
+                log().all().
+                queryParams(parametersMap).
+        when().
+                get("/latest").
+                prettyPeek().
+        then().statusCode(200);
 
     }
 
