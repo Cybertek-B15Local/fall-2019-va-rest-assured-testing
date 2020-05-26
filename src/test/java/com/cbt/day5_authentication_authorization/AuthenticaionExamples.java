15272cb4522dae04e11856780cf473ec9eb3ee7c
marufjont <marufjont@gmail.com>
2020-05-26 19:56:00 -0400

    auth examples

--- src/test/java/com/cbt/day5_authentication_authorization/AuthenticaionExamples.java
+++ src/test/java/com/cbt/day5_authentication_authorization/AuthenticaionExamples.java
@@ -1,5 +1,6 @@
 package com.cbt.day5_authentication_authorization;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -28,12 +29,28 @@ public class AuthenticaionExamples {
     public void apiKeyExample(){
         given().
                 queryParam("apiKey", "e0484f01").
-                queryParam("t", "That Thing called Tadhana").
+                queryParam("t", "Kung Fury").
         when().
                 get("https://omdbapi.com").
                 prettyPeek().
         then().
                 statusCode(200);
+    } // 7.35
+
+
+
+    @Test
+    public void testLogin(){
+        RestAssured.baseURI="http://library2.cybertekschool.com/rest/v1";
+        given().
+                log().all().
+                formParam("email", "librarian16@library").
+                formParam("password", "8BzUhhaU").
+        when().
+                post("/login").
+                prettyPeek().
+                then().statusCode(200);
     }
+
 
 }
