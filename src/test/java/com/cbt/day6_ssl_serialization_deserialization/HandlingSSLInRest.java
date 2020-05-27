9572963bd1e14ac47a0289a63895d0e5af0119e7
marufjont <marufjont@gmail.com>
2020-05-27 19:19:24 -0400

    truststore, keystore

--- src/test/java/com/cbt/day6_ssl_serialization_deserialization/HandlingSSLInRest.java
+++ src/test/java/com/cbt/day6_ssl_serialization_deserialization/HandlingSSLInRest.java
@@ -3,19 +3,47 @@ package com.cbt.day6_ssl_serialization_deserialization;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class HandlingSSLInRest {
 
 
     @Test
     public void badSsl(){
-        RestAssured.
-                given().
+        given().
                         relaxedHTTPSValidation().
                 when().get("https://untrusted-root.badssl.com/").
                         prettyPeek().
                 then().
                         statusCode(200);
     }
+
+    /*
+    truststore -->  it holds the list of certificates
+    keystore -->  it holds the list of keys
+     */
+    @Test
+    public void trustStore(){
+        given().
+                trustStore("/path/to/file", "password").
+         when().
+                get("/my/api").
+        then().statusCode(200);
+    }
+
+    /*
+   truststore -->  it holds the list of certificates
+   keystore -->  it holds the list of keys
+    */
+    @Test
+    public void keyStore(){
+        given().
+                keyStore("/path/to/file", "password").
+        when().
+                get("/my/api").
+        then().statusCode(200);
+    }
+
 
 
 
