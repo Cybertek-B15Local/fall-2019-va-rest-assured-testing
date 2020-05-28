0c35ffb53a92fb82199ac10f7ed7ea70a4bbf109
marufjont <marufjont@gmail.com>
2020-05-28 19:48:30 -0400

    post examples

--- src/test/java/com/cbt/tests/day7_more_serialization_deserialization_post_delete/GetAndPostExample.java
+++ src/test/java/com/cbt/tests/day7_more_serialization_deserialization_post_delete/GetAndPostExample.java
@@ -2,7 +2,9 @@ package com.cbt.tests.day7_more_serialization_deserialization_post_delete;
 
 import com.cbt.pojos.Spartan;
 import com.cbt.utilities.ConfigurationReader;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -30,4 +32,36 @@ public class GetAndPostExample {
 
         System.out.println("spartan.getName() = " + spartan.getName());
     }
+
+
+    @Test
+    public void testPost(){
+        // create a spartan pojo with some random name, gender, phone number
+        Faker faker = new Faker();
+        String name = faker.name().firstName();
+        String gender;
+        if (faker.bool().bool()) {
+            gender = "Female";
+        } else {
+            gender="Male";
+        }
+        String phone = faker.number().digits(10);
+
+        Spartan newSpartan = new Spartan(name, gender,phone);
+        System.out.println("newSpartan = " + newSpartan);
+
+        given().
+                auth().basic("admin", "admin").
+                contentType(ContentType.JSON).
+                body(newSpartan).
+                log().all().
+        when().
+                post("/api/spartans/").prettyPeek().
+        then().
+                statusCode(201);
+
+        // break 7.44
+
+    }
+
 }
