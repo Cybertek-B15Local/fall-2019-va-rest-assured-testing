afdfb3e7f902d5bbe1e27849e53d9bbd29905fd4
marufjont <marufjont@gmail.com>
2020-05-28 18:31:11 -0400

    add properties file

--- src/test/java/com/cbt/pojos/Spartan.java
+++ src/test/java/com/cbt/pojos/Spartan.java
@@ -7,6 +7,12 @@ public class Spartan {
     private String gender;
     private String phone;
 
+    public Spartan(String name, String gender, String phone) {
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
     @Override
     public String toString() {
         return "Spartan{" +
@@ -15,5 +21,37 @@ public class Spartan {
                 ", gender='" + gender + '\'' +
                 ", phone=" + phone +
                 '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
