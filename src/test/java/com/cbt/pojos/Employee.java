839d32cf98a0cd0660a30a310f52b805b1809d3c
marufjont <marufjont@gmail.com>
2020-05-30 12:24:03 -0400

    last donut

--- src/test/java/com/cbt/pojos/Employee.java
+++ src/test/java/com/cbt/pojos/Employee.java
@@ -1,66 +1,180 @@
 package com.cbt.pojos;
 
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Employee {
-    /*
-    {
-    "employee_id": 100,
-    "first_name": "Steven",
-    "last_name": "King",
-    "email": "SKING",
-    "phone_number": "515.123.4567",
-    "hire_date": "2003-06-17T04:00:00Z",
-    "job_id": "AD_PRES",
-    "salary": 24000,
-    "commission_pct": null,
-    "manager_id": null,
-    "department_id": 90,
-    "links": [
-        {
-            "rel": "self",
-            "href": "http://54.146.89.247:1000/ords/hr/employees/100"
-        },
-        {
-            "rel": "edit",
-            "href": "http://54.146.89.247:1000/ords/hr/employees/100"
-        },
-        {
-            "rel": "describedby",
-            "href": "http://54.146.89.247:1000/ords/hr/metadata-catalog/employees/item"
-        },
-        {
-            "rel": "collection",
-            "href": "http://54.146.89.247:1000/ords/hr/employees/"
-        }
-    ]
-}
+
+    @SerializedName("employee_id")
+    
+    private int employeeId;
+    @SerializedName("first_name")
+    
+    private String firstName;
+    @SerializedName("last_name")
+    
+    private String lastName;
+    @SerializedName("email")
+    
+    private String email;
+    @SerializedName("phone_number")
+    
+    private String phoneNumber;
+    @SerializedName("hire_date")
+    
+    private String hireDate;
+    @SerializedName("job_id")
+    
+    private String jobId;
+    @SerializedName("salary")
+    
+    private int salary;
+    @SerializedName("commission_pct")
+    
+    private Object commissionPct;
+    @SerializedName("manager_id")
+    
+    private Object managerId;
+    @SerializedName("department_id")
+    
+    private int departmentId;
+    @SerializedName("links")
+    
+    private List<Link> links = null;
+
+    /**
+     * No args constructor for use in serialization
+     *
      */
+    public Employee() {
+    }
 
-    private String employee_id;
-    private String first_name;
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param jobId
+     * @param hireDate
+     * @param phoneNumber
+     * @param departmentId
+     * @param employeeId
+     * @param links
+     * @param managerId
+     * @param salary
+     * @param email
+     * @param commissionPct
+     */
+    public Employee(int employeeId, String firstName, String lastName, String email, String phoneNumber, String hireDate, String jobId, int salary, Object commissionPct, Object managerId, int departmentId, List<Link> links) {
+        super();
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hireDate = hireDate;
+        this.jobId = jobId;
+        this.salary = salary;
+        this.commissionPct = commissionPct;
+        this.managerId = managerId;
+        this.departmentId = departmentId;
+        this.links = links;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Object getCommissionPct() {
+        return commissionPct;
+    }
+
+    public void setCommissionPct(Object commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public Object getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Object managerId) {
+        this.managerId = managerId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/test/java/com/cbt/pojos/Link.java
@@ -0,0 +1,32 @@
+package com.cbt.pojos;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Link {
+
+    @SerializedName("rel")
+    private String rel;
+    @SerializedName("href")
+    private String href;
+
+    /**
+     * No args constructor for use in serialization
+     */
+    public Link() {
+    }
+
+    /**
+     * @param rel
+     * @param href
+     */
+    public Link(String rel, String href) {
+        super();
+        this.rel = rel;
+        this.href = href;
+    }
+
+    public String getRel() {
+        return rel;
+    }
+}
