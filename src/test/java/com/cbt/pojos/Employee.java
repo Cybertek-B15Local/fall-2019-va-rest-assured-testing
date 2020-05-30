8690ec0d98822b1fc6a8a78b1aab95deb89da442
marufjont <marufjont@gmail.com>
2020-05-30 11:31:33 -0400

    docnut class

---
+++ src/test/java/com/cbt/pojos/Employee.java
@@ -0,0 +1,66 @@
+package com.cbt.pojos;
+
+public class Employee {
+    /*
+    {
+    "employee_id": 100,
+    "first_name": "Steven",
+    "last_name": "King",
+    "email": "SKING",
+    "phone_number": "515.123.4567",
+    "hire_date": "2003-06-17T04:00:00Z",
+    "job_id": "AD_PRES",
+    "salary": 24000,
+    "commission_pct": null,
+    "manager_id": null,
+    "department_id": 90,
+    "links": [
+        {
+            "rel": "self",
+            "href": "http://54.146.89.247:1000/ords/hr/employees/100"
+        },
+        {
+            "rel": "edit",
+            "href": "http://54.146.89.247:1000/ords/hr/employees/100"
+        },
+        {
+            "rel": "describedby",
+            "href": "http://54.146.89.247:1000/ords/hr/metadata-catalog/employees/item"
+        },
+        {
+            "rel": "collection",
+            "href": "http://54.146.89.247:1000/ords/hr/employees/"
+        }
+    ]
+}
+     */
+
+    private String employee_id;
+    private String first_name;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
