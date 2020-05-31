b8fd186c2915fb530bc08150e91597338b44ed93
marufjont <marufjont@gmail.com>
2020-05-31 11:46:19 -0400

    added schema validator and xsd file

--- src/test/java/com/cbt/tests/day9_schema_validation_more_api_tests/SchemaValidationTests.java
+++ src/test/java/com/cbt/tests/day9_schema_validation_more_api_tests/SchemaValidationTests.java
@@ -1,7 +1,10 @@
 package com.cbt.tests.day9_schema_validation_more_api_tests;
 
+import com.cbt.pojos.Spartan;
 import com.cbt.utilities.ConfigurationReader;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,10 @@ import java.io.File;
 
 import static io.restassured.RestAssured.*;
 
+/*
+json schema --> document that describes a json file, it tells how the json must be structured, what fields it will have, what data types they will have
+xsd file --> document that describes an xml file, it tells how the xml must be structured, what fields it will have, what data types they will have
+ */
 public class SchemaValidationTests {
 
     @BeforeAll
@@ -46,5 +53,32 @@ public class SchemaValidationTests {
 
     }
 
+
+    @Test
+    public void validatePostSpartanResponse(){
+        // create spartan pojo object
+        Faker faker = new Faker();
+        String gender = faker.bool().bool()? "Male" : "Female";
+        Spartan spartan = new Spartan(faker.name().firstName(), gender, faker.number().digits(10));
+        System.out.println("spartan = " + spartan);
+
+        // attach the pojo in the post request
+        given().
+                contentType(ContentType.JSON).
+                body(spartan).
+        when().
+                post("api/spartans").prettyPeek().
+        then().
+                statusCode(201).
+                body(JsonSchemaValidator.matchesJsonSchemaInClasspath("post-spartan-schema.json"));
+
+        // validate
+    }
+
+
+    @Test
+    public  void validateXMLResponse(){
+
+    }
 
 }
