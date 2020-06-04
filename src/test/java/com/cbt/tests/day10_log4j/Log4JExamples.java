0a2a39204e0f7078a12bbfac2adc2b9a47f1acce
marufjont <marufjont@gmail.com>
2020-06-04 19:30:16 -0400

    added log4j

--- src/test/java/com/cbt/tests/day10_log4j/Log4JExamples.java
+++ src/test/java/com/cbt/tests/day10_log4j/Log4JExamples.java
@@ -1,5 +1,6 @@
 package com.cbt.tests.day10_log4j;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
@@ -7,10 +8,26 @@ import org.junit.jupiter.api.Test;
 public class Log4JExamples {
 
     private static final Logger logger = LogManager.getLogger(Log4JExamples.class);
+
     @Test
-    public void testLogger(){
+    public void testLogger() {
         System.out.println("wooden spoon");
         logger.info("wooden spoon");
         logger.info("hello world");
     }
+
+
+    @Test
+    public void anotherOne() {
+        logger.setLevel(Level.TRACE);
+
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        logger.fatal("fatal");
+    }
+
+    // BREAK 7.43
 }
