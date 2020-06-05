7de632ef50f4163e07a7024f8c2c9dc13d452db5
marufjont <marufjont@gmail.com>
2020-06-04 20:07:35 -0400

    added log4j

--- src/test/java/com/cbt/tests/day10_log4j/Log4JExamples.java
+++ src/test/java/com/cbt/tests/day10_log4j/Log4JExamples.java
@@ -1,5 +1,7 @@
 package com.cbt.tests.day10_log4j;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -30,4 +32,21 @@ public class Log4JExamples {
     }
 
     // BREAK 7.43
+    // logger object --> captures the messages and passes to appenders for each output type
+    // appender -->  specifci to each output type. captupes thre messages and passes to layout
+    // layout --> writes the message based on given layout format
+
+    @Test
+    public void testThatUsesThatLoggerThing() {
+        logger.info("making api call");
+
+        Response response = RestAssured.
+                when().get("http://api.openrates.io/latest");
+        logger.info("done making api call");
+        logger.debug(response.asString());
+        logger.info("verifying status code");
+        logger.debug(response.statusCode());
+        response.then().statusCode(200);
+        logger.info("test passed");
+    }
 }
