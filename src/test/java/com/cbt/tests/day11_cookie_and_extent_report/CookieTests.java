610d8302c4c899aa07ceeff17a8692e1b706dbc9
marufjont <marufjont@gmail.com>
2020-06-09 19:59:20 -0400

    more cookies

--- src/test/java/com/cbt/tests/day11_cookie_and_extent_report/CookieTests.java
+++ src/test/java/com/cbt/tests/day11_cookie_and_extent_report/CookieTests.java
@@ -16,7 +16,7 @@ public class CookieTests {
      */
     @Test
     public void getCookieTest(){
-        // GET THE COOKIE
+        // GET THE COOKIE USING LOGIN
         Response postResponse =
                 given().
                     formParam("user_login", "username").
@@ -28,9 +28,29 @@ public class CookieTests {
         // getDetailedCookie  --> returns the cookie with given name
         Cookie cookie = postResponse.getDetailedCookie("JSESSIONID");
         System.out.println("cookie = " + cookie);
+        System.out.println("cookie.getName() = " + cookie.getName());
+        System.out.println("cookie.getValue() = " + cookie.getValue());
 
-        // LOGIN USING THE COOKIE
+        // ACCESS THE APP USING THE COOKIE
         // send the request with cookie attached
+        given().
+                cookie(cookie).
+        when().
+                get("http://zero.webappsecurity.com/bank/online-statements.html").
+                prettyPeek().
+        then().
+                statusCode(200).
+                body(not(containsString("login")));
+    }
+
+    @Test
+    public void makeACookie(){
+        // MAKE A COOKIE
+        // cookies expire so if this does nto work that means you need to change the cookie value
+        // you can get the cookie value from the output of the previous example  on top
+        Cookie cookie = new Cookie.Builder("JSESSIONID", "4CB3F51A").build();
+
+        // ACCESS THE APP USING THE COOKIE
         given().
                 cookie(cookie).
         when().
