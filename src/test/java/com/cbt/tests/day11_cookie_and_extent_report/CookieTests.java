5e0d2afdaa22399f876cbd6a75566b70d39b7a71
marufjont <marufjont@gmail.com>
2020-06-09 21:06:13 -0400

    extent config

--- src/test/java/com/cbt/tests/day11_cookie_and_extent_report/CookieTests.java
+++ src/test/java/com/cbt/tests/day11_cookie_and_extent_report/CookieTests.java
@@ -3,6 +3,7 @@ package com.cbt.tests.day11_cookie_and_extent_report;
 import com.cbt.utilities.ExtentConfig;
 import io.restassured.http.Cookie;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -17,19 +18,22 @@ public class CookieTests {
     login to zero bank app using form params
         user_login: username and user_password: password
      */
+    @DisplayName("Get a cookie and access the reports using cookie")
     @Test
-    public void getCookieTest(){
+    public void getCookieTest() {
         // GET THE COOKIE USING LOGIN
+        ExtentConfig.test.info("send request to get a cookie");
         Response postResponse =
                 given().
-                    formParam("user_login", "username").
-                    formParam("user_password", "password").
-                    log().all().
-                when().
-                    post("http://zero.webappsecurity.com/signin.html");
+                        formParam("user_login", "username").
+                        formParam("user_password", "password").
+                        log().all().
+                        when().
+                        post("http://zero.webappsecurity.com/signin.html");
         // cookie is returned as part of teh response.
         // getDetailedCookie  --> returns the cookie with given name
         Cookie cookie = postResponse.getDetailedCookie("JSESSIONID");
+        ExtentConfig.test.info("cookie = " + cookie.toString());
         System.out.println("cookie = " + cookie);
         System.out.println("cookie.getName() = " + cookie.getName());
         System.out.println("cookie.getValue() = " + cookie.getValue());
@@ -38,30 +42,32 @@ public class CookieTests {
         // send the request with cookie attached
         given().
                 cookie(cookie).
-        when().
+                when().
                 get("http://zero.webappsecurity.com/bank/online-statements.html").
                 prettyPeek().
-        then().
+                then().
                 statusCode(200).
                 body(not(containsString("login")));
     }
 
+    @DisplayName("Make your own a cookie and access the reports using cookie")
     @Test
-    public void makeACookie(){
+    public void makeACookie() {
         // MAKE A COOKIE
         // cookies expire so if this does nto work that means you need to change the cookie value
         // you can get the cookie value from the output of the previous example  on top
         Cookie cookie = new Cookie.Builder("JSESSIONID", "4CB3F51A").build();
-
+        ExtentConfig.test.info("Cookie: " + cookie.toString());
         // ACCESS THE APP USING THE COOKIE
         given().
                 cookie(cookie).
-        when().
+                when().
                 get("http://zero.webappsecurity.com/bank/online-statements.html").
                 prettyPeek().
-        then().
+                then().
                 statusCode(200).
                 body(not(containsString("login")));
+        ExtentConfig.test.info("done");
     }
 
 }
