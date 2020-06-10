3aade8b4ef0248a9041f32ebf3b1788f9f71717c
marufjont <marufjont@gmail.com>
2020-06-10 19:16:09 -0400

    put vs patch

--- src/test/java/com/cbt/tests/day12_could_be_last_day/PatchVsPutExample.java
+++ src/test/java/com/cbt/tests/day12_could_be_last_day/PatchVsPutExample.java
@@ -1,7 +1,9 @@
 package com.cbt.tests.day12_could_be_last_day;
 
 import com.cbt.pojos.Spartan;
+import com.cbt.utilities.ConfigurationReader;
 import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -15,22 +17,22 @@ public class PatchVsPutExample {
      * CRUD --> create, read,   update,     delete
      * SQL -->  insert, select, update,     delete
      * REST --> post,   get,    put/patch,  delete
-     *
+     * <p>
      * post --> new row/record is added to the database
      * get --> no changes in the database
      * put --> when updating with put we need to provide the whole information,
-     *          new row is added to the database with the updated information
-     *
+     * new row is added to the database with the updated information
+     * <p>
      * patch  --> we only provide the information which we want to update and also id information
      * delete  --> a row / record is removed from database
-     *
+     * <p>
      * PUT
      * id fname lname title
      * 1 luba  n     another ceo
      * 2 alina n    another another ceo
      * 3 alina p    another another ceo
-     *
-     *
+     * <p>
+     * <p>
      * PATCH
      * id fname lname title
      * 1  luba  n     another ceo
@@ -38,7 +40,8 @@ public class PatchVsPutExample {
      */
 
     @Test
-    public void put (){
+    public void put() {
+        RestAssured.baseURI = ConfigurationReader.getProperty("spartan_base_url");
         // create pojo
         Faker faker = new Faker();
         String name = faker.name().firstName();
@@ -46,12 +49,13 @@ public class PatchVsPutExample {
         if (faker.bool().bool()) {
             gender = "Female";
         } else {
-            gender="Male";
+            gender = "Male";
         }
         String phone = faker.number().digits(10);
         Spartan spartan = new Spartan(name, gender, phone);
 
         // create new spartan, verify 201, save id
+
         Response postResponse =
                 given().
                         log().all().
@@ -66,11 +70,33 @@ public class PatchVsPutExample {
         System.out.println("id = " + id);
 
         // get the spartan information using the id
-        Response getResponse= given().
+        Response getResponse = given().
                 auth().basic("admin", "admin").
                 pathParam("id", id).
                 when().
                 get("api/spartans/{id}").prettyPeek();
         getResponse.then().statusCode(200);
+
+        // UPDATE spartan info
+        spartan.setName(faker.name().fullName());
+
+        given().
+                auth().basic("admin", "admin").
+                pathParam("id", id).
+                contentType(ContentType.JSON).
+            body(spartan).
+                when().
+                put("/api/spartans/{id}").prettyPeek().
+            then().
+                statusCode(204);
+        // GET THE NEW SPARTAN INFORMATION
+
+        getResponse = given().
+                auth().basic("admin", "admin").
+                pathParam("id", id).
+                when().
+                get("api/spartans/{id}").prettyPeek();
+        getResponse.then().statusCode(200);
+
     }
 }
