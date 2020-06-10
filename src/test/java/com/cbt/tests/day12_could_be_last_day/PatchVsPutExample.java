b17b94225e44210e8461bd4174a09f7feca14f75
marufjont <marufjont@gmail.com>
2020-06-10 19:04:40 -0400

    put vs patch

---
+++ src/test/java/com/cbt/tests/day12_could_be_last_day/PatchVsPutExample.java
@@ -0,0 +1,76 @@
+package com.cbt.tests.day12_could_be_last_day;
+
+import com.cbt.pojos.Spartan;
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class PatchVsPutExample {
+
+    /**
+     * CRUD --> create, read,   update,     delete
+     * SQL -->  insert, select, update,     delete
+     * REST --> post,   get,    put/patch,  delete
+     *
+     * post --> new row/record is added to the database
+     * get --> no changes in the database
+     * put --> when updating with put we need to provide the whole information,
+     *          new row is added to the database with the updated information
+     *
+     * patch  --> we only provide the information which we want to update and also id information
+     * delete  --> a row / record is removed from database
+     *
+     * PUT
+     * id fname lname title
+     * 1 luba  n     another ceo
+     * 2 alina n    another another ceo
+     * 3 alina p    another another ceo
+     *
+     *
+     * PATCH
+     * id fname lname title
+     * 1  luba  n     another ceo
+     * 2  alina n    another another ceo
+     */
+
+    @Test
+    public void put (){
+        // create pojo
+        Faker faker = new Faker();
+        String name = faker.name().firstName();
+        String gender;
+        if (faker.bool().bool()) {
+            gender = "Female";
+        } else {
+            gender="Male";
+        }
+        String phone = faker.number().digits(10);
+        Spartan spartan = new Spartan(name, gender, phone);
+
+        // create new spartan, verify 201, save id
+        Response postResponse =
+                given().
+                        log().all().
+                        auth().basic("admin", "admin").
+                        contentType(ContentType.JSON).
+                        body(spartan).
+                        when().
+                        post("/api/spartans").prettyPeek();
+        postResponse.then().statusCode(201).and().body("success", is("A Spartan is Born!"));
+
+        int id = postResponse.path("data.id");
+        System.out.println("id = " + id);
+
+        // get the spartan information using the id
+        Response getResponse= given().
+                auth().basic("admin", "admin").
+                pathParam("id", id).
+                when().
+                get("api/spartans/{id}").prettyPeek();
+        getResponse.then().statusCode(200);
+    }
+}
